7aa52635b06dc6d566ea256a3851ad10c78f07fc
chechtberger <c.hechtberger@gmail.com>
2016-12-20 13:02:28 +0100

    Test Stephan geb mal bescheid!

--- src/application/Client.java
+++ src/application/Client.java
@@ -1,5 +1,8 @@
 package application;
 
 public class Client {
-
+	
+	public stativ void main (String[]args){
+		System.out.println("Test");
+	}
 }
--- src/application/ISpiel.java
+++ src/application/ISpiel.java
@@ -2,4 +2,5 @@ package application;
 
 public interface ISpiel {
 
+	
 }
