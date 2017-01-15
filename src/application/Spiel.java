b6ee6b4e0f47bb7b0a98d35adc05001fbdad9db1
oliverdu <oliverdu@192.168.2.110>
2017-01-15 16:11:18 +0100

    Spielablauf

--- src/application/Spiel.java
+++ src/application/Spiel.java
@@ -55,10 +55,29 @@ public class Stapel {
 
 }
 
+Spielablauf:
 
+Spieler 1,2;
+Kartenstapel;
+Kartenstapel mischen;
+Kartenstapel teilen;
+Spieler 1 Kartenstapel links
+Spieler 2 Kartenstapel rechts
 
+Spieler1 wählt Elementattribut erster Karte Stapel links;
+	Attribut wird verglichen mit Attribut erster Karte Stapel rechts;
+	
+wenn Spieler 1 gewonnen dann wie vorher und Spieler 1 bekommt Karte von Spieler 2;
 
+wenn Spieler 2 gewonnen dann; 
 
+Spieler2 wählt Elementattribut neuer erster Karte Stapel rechts;
+	Attribut wird verglichen mit Attribut erster neuer Karte Stapel links;
+	
+wenn Spieler 2 gewonnen dann wie vorher und Spieler 2 bekommt Karte von Spieler 1;
+
+wenn Stapel links 	leer, Spieler2 gewonnen;
+wenn Stapel rechts	leer, Spieler1 gewonnen;
 
 
 */
