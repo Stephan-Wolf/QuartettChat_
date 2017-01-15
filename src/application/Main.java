42bf04e8df19f14ac187cecf6f4900dd7188664d
chechtberger <c.hechtberger@gmail.com>
2017-01-15 16:58:10 +0100

    Main und ViewModel aktualisiert

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,16 +4,19 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(new Scene(root));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -22,5 +25,11 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+		System.out.println("TeST");
 	}
 }
+
+
+
+
+
--- src/application/ViewModel.java
+++ src/application/ViewModel.java
@@ -1,5 +1,66 @@
 package application;
 
+import java.io.File;
+import java.lang.reflect.Array;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
 public class ViewModel {
+	
+	@FXML
+	private Text label1, label2, label3, label4, label5;
+	
+	@FXML
+	private ImageView imageDisplay;
+	
+	@FXML
+	private AnchorPane setImage;
+	
+	Karte karte = new Karte("Audi-V8","file:///Users/christianhechtberger/git/QuartettChat_/src/application/Img/Audi-V8.jpg", "500", "250","16.5","6500","4");
+	
+	
+//	Image image = new Image("file:///Users/christianhechtberger/git/QuartettChat_/src/application/Img/BMWZ1.jpg");
+	Image image2 = new Image("file:///Users/christianhechtberger/git/QuartettChat_/src/application/Img/Audi-V8.jpg");
+	
+	
+	
+	@FXML
+	private void compare(ActionEvent event){
+//		System.out.println("Test");
+//		label1.setText(karte2.getPs());
+//		karte1.getPs().bind(label1.textProperty());
+//		label1.textProperty().bind(karte.getPs());
+//		
+//    	label2.setText(karte.getKmh());
+//		label3.setText(karte.getVerbrauch());
+//		label4.setText(karte.getCcm());
+//		label5.setText(karte.getBeschleunigung());
+//		imageDisplay.setImage(image2);
+//		System.out.println(karte.getPs());
+//		Karte karte = new Karte("file:///Users/christianhechtberger/git/QuartettChat_/src/application/Img/Audi-V8.jpg", "400", "230","15","6550","2");
+	}
+	@FXML
+	private void nextCard(ActionEvent event){
+		label1.textProperty().bind(karte.psProperty());
+		label2.textProperty().bind(karte.kmhProperty());
+		label3.textProperty().bind(karte.verbrauchProperty());
+		label4.textProperty().bind(karte.ccmProperty());
+		label5.textProperty().bind(karte.beschleunigungProperty());
+		Image image = new Image(karte.getImage());
+		imageDisplay.setImage(image);	
+		
+	}
+
 
 }
+
