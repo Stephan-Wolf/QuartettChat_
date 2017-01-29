2c59f4a51517c2a572dc133d758c89e3cab61dc2
Stephan Wolf <stephan-wolf@gmx.de>
2017-01-29 18:45:38 +0100

    TEst

--- src/app/Beobachter.java
+++ src/app/Beobachter.java
@@ -2,7 +2,7 @@ package app;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-
+// TEST
 public interface Beobachter extends Remote {
 
 	void update (String name, String ps, String kmh, String verbrauch, String ccm, String beschleunigung, boolean aktiverSpieler_1)  throws RemoteException;
