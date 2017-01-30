809465664004c968f282c68a6b022cabc4167433
chechtberger <c.hechtberger@gmail.com>
2017-01-30 11:02:04 +0100

    Refactoring kartenstapel

--- src/app/Kartenstapel.java
+++ src/app/Kartenstapel.java
@@ -26,10 +26,6 @@ import java.util.Collections;
 	Karte a14 = new Karte("Mitsubishi_Lancer_WRC","/Img/Mitsubishi_Lancer_WRC.jpg","300","280", "15","1999","4.4");
 	Karte a15 = new Karte("Skoda_Fabia_WRC","/Img/Skoda_Fabia_WRC.jpg","295","275","17","1999","4.6");
 	Karte a16 = new Karte("Opel_Vectra_GTS","/Img/Opel_Vectra_GTS.jpg","465","285", "21","3998","4.4");
-//	Karte a13 = new Karte("Ferrari F40","/Img/FerrariF40.jpg","521","325", "18","4706","3.7");
-//	Karte a14 = new Karte("Mitsubishi Lancer WRC","/Img/Mitsubishi_Lancer_WRC.jpg","300","280", "15","1999","4.4");
-//	Karte a15 = new Karte("Skoda Fabia WRC","/Img/Skoda_Fabia_WRC.jpg","295","275","17","1999","4.6");
-//	Karte a16 = new Karte("Opel Vectra GTS","/img/Opel_Vectra_GTS.jpg","465","285", "21","3998","4.4");
 
 	
 	//  Karte (String name, String bildUrl, String ps, String kmh, String verbrauch, String ccm, String beschleunigung)
@@ -52,38 +48,7 @@ import java.util.Collections;
 		list.add(a15);
 		list.add(a16);
 		
-//        list.add(new Karte("Audi_A4_DTM", "bildUrl", "465", "290", "20", "4000", "4.3"));
-//        list.add(new Karte("Chrysler_Viper_GTSR","bildUrl", "600", "315", "30","7986", "3.7"));
-//        list.add(new Karte("Citroen_Xsara_WRC","bildUrl", "305", "270", "18","1998", "4.7"));
-//        list.add(new Karte("VW_Polo_Super1600","bildUrl", "215", "205", "12","1598","5.8"));
-//        list.add(new Karte("Opel_Vectra_GTS","bildUrl","465","285", "21","3998","4.4"));
-//        list.add(new Karte("Bentley_EXP_Speed8","bildUrl","620","335","32","3994","3.8"));
-//        list.add(new Karte("Skoda_Fabia_WRC","bildUrl","295","275","17","1999","4.6"));
-//        list.add(new Karte("BMW_320i_WTCC","bildUrl","275","250", "19","1998","4.4"));
-//        list.add(new Karte("Nissan_350Z_JGTC","bildUrl","465","330", "16","2987","4.1"));
-//        list.add(new Karte("Maserati_MC12","bildUrl","630","335","22","5998","3.8"));
-//        list.add(new Karte("Mitsubishi_Lancer_WRC","bildUrl","300","280", "15","1999","4.4"));
-//        list.add(new Karte("Lister_Storm_GT","bildUrl","630","325","33","6996","3.7"));
-//        list.add(new Karte("Mercedez_C_AMG","bildUrl","465","295","14","4000","4.1"));
-//        list.add(new Karte("Porsche_911_Carrera_RS","bildUrl","280","280", "14","3600","4.8"));
-//        list.add(new Karte("Subaru_Impreza_WRC","bildUrl","310","285","18","1998","4.8"));
-//        list.add(new Karte("Cadillac_CTSV","bildUrl","500","265","35","6026","4.9"));
-//        list.add(new Karte("Pescarolo_Judd_LMP1","bildUrl","630","385","25","4997","3.0"));
-//        list.add(new Karte("TVR_Tuscan_400R","bildUrl","446","330","25","3996","3.9"));
-//        list.add(new Karte("Volvo_S60","bildUrl","285","280","12","2400","4.6"));
-//        list.add(new Karte("Chevrolet_Lacetti_WTCC","bildUrl","280","254","18","1998","4.5"));
-//        list.add(new Karte("Vauxhall_Astra","bildUrl","335","298","25","3998","3.9"));
-//        list.add(new Karte("AlflaRomeo_147_GTACup","bildUrl","220","230","14","1970","5.6"));
-//        list.add(new Karte("Morgan_Aero_GT","bildUrl","500","330","31","4398","4.1"));
-//        list.add(new Karte("Suzuki_Ignis","bildUrl","215","225","17","1598","6.0"));
-//        list.add(new Karte("Porsche_996_GT3_RSR","bildUrl","451","295","16","3598","3.1"));
-//        list.add(new Karte("Audi_R8","bildUrl","593","330","14","3600","3.3"));
-//        list.add(new Karte("Porsche_911_GT1","bildUrl","600","355","16","3200","3.5"));
-//        list.add(new Karte("Heiko_HS4_ODIN","bildUrl","354","275","12","2521","5.5"));
-//        list.add(new Karte("Peugeot_307_WRC","bildUrl","298","290","18","1998","4.3"));
-//        list.add(new Karte("Seat_Leon_Supercopa","bildUrl","254","235", "11","1797","4.8"));
-//        list.add(new Karte("Ford_Focus_WRC","bildUrl","300","205", "17","1998","3.9"));
-//        list.add(new Karte("Ferrari_F40","bildUrl","521","325", "18","4706","3.7"));
+
 	}
 	
 	public Karte getKarte(int id){
