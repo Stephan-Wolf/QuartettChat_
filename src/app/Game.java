981e899e81658043fa4cd77af76d147091050cff
hultaaj <hultaaj@gmail.com>
2017-03-08 09:36:14 +0100

    redundante Zeile in der Methode updatePlayerCardStacks() mittels der neuen Methode transferCard() beseitigt

--- src/app/Game.java
+++ src/app/Game.java
@@ -26,82 +26,24 @@ public class Game extends UnicastRemoteObject implements IGame {
 	final String CCM = "ccm";
 	final String ACCELERATION = "beschleunigung";
 	
-	// boolean aktiverSpieler1Boolean;
 	private Player player1;
 	private Player player2;
 	private GameCardStack cardStack;
-//	private int players1NumberOfCardsInt;
-//	private int players2NumberOfCardsInt;
 	private boolean gameover = false;
 	
 	private BooleanProperty player1IsActive = new SimpleBooleanProperty();
 	
-	// Werte der aufgedeckten Karte vom Spieler1
-	// private StringProperty spieler1Name =  new SimpleStringProperty();
-	// private StringProperty spieler1Ps =  new SimpleStringProperty();
-	// private StringProperty spieler1Kmh =  new SimpleStringProperty();
-	// private StringProperty spieler1Verbrauch =  new SimpleStringProperty();
-	// private StringProperty spieler1Ccm =  new SimpleStringProperty();	
-	// private StringProperty spieler1Beschleunigung =  new SimpleStringProperty();
-	// private StringProperty spieler1JpgUrl =  new SimpleStringProperty();
+
 	private StringProperty spieler1Status = new SimpleStringProperty();
 	
-	
-	// private ObjectProperty<javafx.scene.image.Image> spieler1Img = new SimpleObjectProperty<>();
-	// Anzahl der Karten vom Spieler 1 als StringProperty
-	// private StringProperty spieler1Kartenanzahl = new SimpleStringProperty();
-	
-
-	// Werte der aufgedeckten Karte vom Spieler2
-	// private StringProperty spieler2Name =  new SimpleStringProperty();
-	// private StringProperty spieler2Ps =  new SimpleStringProperty();
-	// private StringProperty spieler2Kmh =  new SimpleStringProperty();
-	// private StringProperty spieler2Verbrauch =  new SimpleStringProperty();
-	// private StringProperty spieler2Ccm =  new SimpleStringProperty();	
-	// private StringProperty spieler2Beschleunigung =  new SimpleStringProperty();
-	// private StringProperty spieler2JpgUrl =  new SimpleStringProperty();
 	private StringProperty spieler2Status = new SimpleStringProperty();
 
-	// private ObjectProperty<javafx.scene.image.Image> spieler2Img = new SimpleObjectProperty<>();
-	// Anzahl der Karten vom Spieler 2 als StringProperty
-	// private StringProperty spieler2Kartenanzahl = new SimpleStringProperty();
-	
 	
 	public Game () throws RemoteException {
 		cardStack = new GameCardStack();
 		player1 = new Player();
 		player2 = new Player();
 		determineGameInitiator();
-		// Spieler1 beginnt
-		// sp�ter �ndern -> nach Zufall
-		//random math #
-		// Rafactoring, in eine Methode packen, verk�rzen
-		// activePlayer1.setValue(aktiverSpieler1Boolean);
-				
-		
-		
-		// sp�ter erstetzen durch eine Funktion der Klasse Spieler, mit der der Spieler
-		// ein array mit den Werten der oberen Karte liefert
-		// und dann werden die Daten im Spiel aktualisiert bzw. an das ViewModel im Rahmen von updates weiter geschickt
-		// spieler1Name.bind(spieler1.obereKarteNameProperty());
-		// spieler1Ps.bind(spieler1.obereKartePsProperty());
-		// spieler1Kmh.bind(spieler1.obereKarteKmhProperty()); 
-		// spieler1Verbrauch.bind(spieler1.obereKarteVerbrauchProperty());
-		// spieler1Ccm.bind(spieler1.obereKarteCcmProperty());
-		// spieler1Beschleunigung.bind(spieler1.obereKarteBeschleunigungProperty());
-		// spieler1Img.bind(spieler1.obereKarteImgProperty());
-		// spieler1JpgUrl.bind(spieler1.obereKarteJpgUrlProperty());
-		
-		// spieler2Name.bind(spieler2.obereKarteNameProperty());
-		// spieler2Ps.bind(spieler2.obereKartePsProperty());
-		// spieler2Kmh.bind(spieler2.obereKarteKmhProperty()); 
-		// spieler2Verbrauch.bind(spieler2.obereKarteVerbrauchProperty());
-		// spieler2Ccm.bind(spieler2.obereKarteCcmProperty());
-		// spieler2Beschleunigung.bind(spieler2.obereKarteBeschleunigungProperty());
-		// spieler2Img.bind(spieler2.obereKarteImgProperty());
-		// spieler2JpgUrl.bind(spieler2.obereKarteJpgUrlProperty());
-		
-//		spieler1Ps.bind(spieler1.obereKartePsProperty());
 		
 	}
 	
@@ -121,30 +63,13 @@ public class Game extends UnicastRemoteObject implements IGame {
 		this.dealCards();
 	}
 	
-	// ???????????
 	public void repeatGame () {
 		
-		// zuerst die alten  Kartenstapel-Objekt, Spielerstapel-Objekte l�schen?
 		cardStack = new GameCardStack();
 		gameover = false;
 		this.mixCards();
 		this.dealCards();
 		this.determineGameInitiator();
-		// Spieler1 beginnt
-		// sp�ter �ndern -> nach Zufall
-		//random math #
-		// Rafactoring, in eine Methode packen, verk�rzen
-//		int randomNumber;
-//	    randomNumber = (int)(Math.random() * 2+1);
-//	    System.out.println(randomNumber);
-//	    
-//		if (randomNumber == 1){
-//			aktiverSpieler1Boolean = false;
-//		}
-//		else{
-//			aktiverSpieler1Boolean = true;
-//		}
-//		activePlayer1.setValue(aktiverSpieler1Boolean);
 		
 	}
 	
@@ -160,14 +85,12 @@ public class Game extends UnicastRemoteObject implements IGame {
 		this.player1.setPlayerCardStack(playerCardStack[0]);
 		player1.updateNumberOfCards();
 		this.player1.uncoverTopCard();
-		// players1NumberOfCardsInt = playerCardStack[0].getNumberOfCards();
-		//spieler1Kartenanzahl.setValue(String.valueOf(player1.numberOfCardsProperty().getValue())); 
+
 		
 		this.player2.setPlayerCardStack(playerCardStack [1]);
 		player2.updateNumberOfCards();
 		this.player2.uncoverTopCard();
-		// players2NumberOfCardsInt = playerCardStack [1].getNumberOfCards();
-		// spieler2Kartenanzahl.setValue(String.valueOf(player2.numberOfCardsProperty().getValue()));
+
 		System.out.println();
 		System.out.println("kartenAusteilen()");
 		System.out.println("Spielerstapel Spieler1:");
@@ -200,7 +123,6 @@ public class Game extends UnicastRemoteObject implements IGame {
 		}
 	}
 	
-	// AUCHTUNG - NOCH REFACORING -> int ergebnis = liefereVergleichsergebnis(StringProperty1, StringProperty2)
 	private int compareCardAttributes(String cardAttributes){
 		System.out.println("vergleicheAttribut() attribut: " + cardAttributes);
 		
@@ -251,71 +173,16 @@ public class Game extends UnicastRemoteObject implements IGame {
 		}
 	}
 	
-//	private int liefereVergleichsergebnisKleiner(StringProperty attributwert1, StringProperty attributwert2) {
-//		float attributwert1Float = Float.parseFloat(attributwert1.getValue());
-//		float attributwert2Float = Float.parseFloat(attributwert2.getValue());
-//		
-//		if (attributwert1Float < attributwert2Float) {
-//			return ROUNDWINNER_PLAYER_1;
-//		} else if (attributwert2Float < attributwert1Float) {
-//			return ROUNDWINNER_PLAYER_2;
-//		}
-//		else { 
-//			return DRAW;
-//		}
-//	}
-	
-	// Refactoring??
+
 	private void updatePlayerCardStacks(int roundResult) {
 		System.out.println("spielerstapelAktualisieren()");
 		
 		if (roundResult == ROUNDWINNER_PLAYER_1) {
-			
-			// spieler2
-			Card karte = this.player2.giveCard();
-			System.out.println("Aufruf spieler2.gebeKarteZurueck()");
-			// players2NumberOfCardsInt--;
-			player2.updateNumberOfCards();
-			// spieler2Kartenanzahl.setValue(String.valueOf(player2.numberOfCardsProperty().getValue()));
-			
-			if (player2.numberOfCardsProperty().getValue() == 0) {
-				gameover = true;
-			} else {
-				player2.uncoverTopCard();
-			}
-
-			// spieler1
-			this.player1.receiveCard(karte);
-			System.out.println("Aufruf spieler1.empfangeKarte(k)");
-			// players1NumberOfCardsInt++;
-			player1.updateNumberOfCards();
-			// spieler1Kartenanzahl.setValue(String.valueOf(player1.numberOfCardsProperty().getValue()));
-			this.player1.moveTopCardDownwards();
-			player1.uncoverTopCard();
+			this.transferCard(this.player1, this.player2);
 			
 		} else if (roundResult == ROUNDWINNER_PLAYER_2){
+			this.transferCard(this.player2, this.player1);
 			
-			// spieler1
-			Card k = this.player1.giveCard();
-			System.out.println("Aufruf spieler1.gebeKarteZurueck()");
-			// players1NumberOfCardsInt--;
-			player1.updateNumberOfCards();
-			// spieler1Kartenanzahl.setValue(String.valueOf(player1.numberOfCardsProperty().getValue()));
-			if (player1.numberOfCardsProperty().getValue() == 0) {
-				
-				gameover = true;
-			} else {
-				player1.uncoverTopCard();
-			}
-			
-			// spieler2
-			this.player2.receiveCard(k);
-			System.out.println("Aufruf spieler2.empfangeKarte(k)");
-			// players2NumberOfCardsInt++;
-			player2.updateNumberOfCards();
-			// spieler2Kartenanzahl.setValue(String.valueOf(player2.numberOfCardsProperty().getValue()));
-			this.player2.moveTopCardDownwards();
-			player2.uncoverTopCard();
 		}else if (roundResult == ROUNDWINNER_PLAYER_1 && gameover == true){
 			System.out.println("SPiel Over und Gewinner ist Spieler 1");
 		}else if (roundResult == ROUNDWINNER_PLAYER_2 && gameover == true){
@@ -329,6 +196,18 @@ public class Game extends UnicastRemoteObject implements IGame {
 	}
 	
 	private void transferCard (Player winner, Player loser) {
+		Card card = loser.giveCard();
+		loser.updateNumberOfCards();
+		if (loser.numberOfCardsProperty().getValue() == 0) {
+			gameover = true;
+		} else {
+			loser.uncoverTopCard();
+		}
+
+		winner.receiveCard(card);
+		winner.updateNumberOfCards();
+		winner.moveTopCardDownwards();
+		winner.uncoverTopCard();
 		
 	}
 	
@@ -343,11 +222,8 @@ public class Game extends UnicastRemoteObject implements IGame {
 			spieler2Status.setValue("Runde gewonnen");
 			player1IsActive.setValue(false);	
 		}
-		// sp�ter l�schen, wenn wir auf Properties in der Klasse Spiel verzichten
 	}
 	
-	
-	// Refactoring??? read only??
 	public StringProperty players1HpProperty () {
 		return this.player1.hpAttributeOfTopCardProperty();
 	}
@@ -369,17 +245,10 @@ public class Game extends UnicastRemoteObject implements IGame {
 	}
 	
 	
-//	public ObjectProperty<javafx.scene.image.Image> getSpieler1Img () {
-//		return this.spieler1Img;
-//	}
-	
-	
 	public StringProperty players1SourceOfJpgProperty () {
 		return this.player1.jpgSourceOfTopCardProperty();
 	}
 	
-	
-	// Refactoring??? read only??
 	public StringProperty players2HpProperty () {
 		return this.player2.hpAttributeOfTopCardProperty();
 	}
@@ -400,9 +269,6 @@ public class Game extends UnicastRemoteObject implements IGame {
 		return this.player2.accelerationAttributeOfTopCardProperty();
 	}
 	
-//	public ObjectProperty<javafx.scene.image.Image> getSpieler2Img () {
-//		return this.spieler2Img;
-//	}
 	
 	public StringProperty players2SourceOfJpgProperty () {
 		return this.player2.jpgSourceOfTopCardProperty();
